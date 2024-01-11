503523d5aef802e220c6e1f822ac039bad97afc0
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-01-11 09:44:39 -0300

    Add:  Second compoment Refact: FirstComponent

--- src/app/testComponent/firstComponent.component.ts
+++ src/app/testComponent/firstComponent.component.ts
@@ -2,10 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'firstComponent',
-    templateUrl: `
-        <p>FristComponent</p>
-    `
-    //styleUrls: ['./diretivas-customizadas.component.css']
+    templateUrl: './second-component.component.html'
 })
 export class FirstComponent{
 
