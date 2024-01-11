7c88c8a95925fbbc3dcf9df413f424f289ba22b8
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-01-11 09:24:31 -0300

    Add: componente criado manualmente

---
+++ src/app/testComponent/firstComponent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'firstComponent',
+    templateUrl: `
+        <p>FristComponent</p>
+    `
+    //styleUrls: ['./diretivas-customizadas.component.css']
+})
+export class FirstComponent{
+
+}
